6773a6605ebb0f4425b6c78d5fa806ee00b7fc2e
irom77 <aniairom0>
2018-02-28 15:24:55 -0500

    update

--- sensors/item_sensor.py
+++ sensors/item_sensor.py
@@ -53,13 +53,13 @@ class ItemSensor(PollingSensor):
         
         self._logger.info("Found {0} items in {1}".format(items.count(), target))        
         if items.count() > 0:            
-           self._logger.info("Here it is {0}".format(items.values()))
-           
+           self._logger.info("Here it is:\nItems of type {0}:\n{1}".format(type(items), items))
+           """
            for payload in items.values('item_id', 'subject', 'body', 'datetime_received'):
               self._logger.info("Sending trigger for item '{0}'.".format(payload['subject']))
               self._sensor_service.dispatch(trigger='exchange_new_item', payload=payload)
               # self._set_last_date(payload['datetime_received'])
-           
+           """
 
     def cleanup(self):
         # This is called when the st2 system goes down. You can perform cleanup operations like
